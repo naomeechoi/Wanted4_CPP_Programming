2e9355e9df214cc3404ebdd1a166d253eb2ff964
Naomi Choi <futureverever@gmail.com>
2026-01-21 09:58:08 +0900

    Find memory leaks

--- Pointer/Main.cpp
+++ Pointer/Main.cpp
@@ -1,4 +1,14 @@
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
 #include <iostream>
+
+#ifdef _DEBUG
+#define new new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+#else
+#define new new
+#endif
+
 /*void DeletePointer(void* ptr)
 {
 	delete ptr;
@@ -23,8 +33,15 @@ void DeletePointer(void*& ptr)
 
 int main()
 {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	// TODO Å×½ºÆ®
+	int* intPointer = new int[10];
 	void* pointer = new int;
-	DeletePointer(&pointer);
+	DeletePointer(pointer);
+	delete intPointer;
 
+
+	/*_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
+	_CrtDumpMemoryLeaks();*/
 	return 0;
 }
