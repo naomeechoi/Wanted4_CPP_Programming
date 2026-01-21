d239047a4ad1fb4e44749b4fd46af3be1d9f926c
Naomi Choi <futureverever@gmail.com>
2026-01-21 09:22:08 +0900

    Add the pointer project

--- Pointer/Main.cpp
+++ Pointer/Main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+void DeletePointer(void* ptr)
+{
+	delete ptr;
+	ptr = nullptr;
+ }
+
+void DeletePointer(void** ptr)
+{
+	delete *ptr;
+	*ptr = nullptr;
+
+}
+
+void DeletePointer(void*& ptr)
+{
+	if (ptr == nullptr)
+		return;
+
+	delete ptr;
+	ptr = nullptr;
+}
 
 int main()
 {
+	void* pointer = new int;
+	DeletePointer(&pointer);
 
+	return 0;
 }
